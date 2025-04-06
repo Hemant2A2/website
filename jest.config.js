38382850a460028df50084099a64da0294893967
Hemant Ayuj <hemant.ayuj@gmail.com>
2025-04-07 00:34:53 +0530

    added coverage percentage 90%

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,15 @@ const config = {
   collectCoverageFrom: ['scripts/**/*.ts'],
   coveragePathIgnorePatterns: ['scripts/compose.ts', 'scripts/tools/categorylist.ts', 'scripts/tools/tags-color.ts'],
   testMatch: ['**/tests/**/*.test.*', '!**/netlify/**/*.test.*'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
 };
 
 export default config;
